019d94543999695c73a35288e6bcc3f7c1d5fb9a
abe1ge <greenfire@live.co.uk>
2017-02-06 01:39:44 +0000

    first commit

---
+++ NumberReader/NumberReader/Program.cs
@@ -0,0 +1,168 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NumberReader
+{
+    class WithString
+    {
+
+        Dictionary<string, string> units = new Dictionary<string, string>()
+            {
+                {"1","one"},{ "2","two"},{ "3","three"},{"4","four"},{"5","five"},{"6","six"},{"7","seven"},{"8","eight"},{"9","nine"}
+            };
+
+        Dictionary<string, string> teens = new Dictionary<string, string>()
+            {
+                {"10","eleven"},{"11","eleven"},{ "12","twelve"},{ "13","threteen"},{"14","fourteen"},{"15","fifteen"},{"16","sixteen"},{"17","seventeen"},{"18","eighteen"},{"19","nineteen"}
+            };
+
+        Dictionary<string, string> tens = new Dictionary<string, string>()
+            {
+                {"2","twenty"},{"3","thirty" },{"4","fourty" },{"5","fifty" },{"6","sixty" },{"7","seventy" },{"8","eighty" },{"9","ninty" }
+            };
+
+        public string render(string strnum)
+        {
+            string answer = "";
+
+            for (int i = strnum.Length; i > 0; i--)
+            {
+                switch (i)
+                {
+                        
+                    case 2:
+                        if (strnum[strnum.Length - i] == '0')
+                        {
+                            answer += units[strnum[strnum.Length - (i - 1)].ToString()];
+                        }
+                        else if (strnum[strnum.Length - i] == '1')
+                        {
+                            answer += teens[strnum[strnum.Length - i].ToString() + strnum[strnum.Length - (i - 1)].ToString()];
+                        }
+                        else
+                        {
+                            answer += tens[strnum[strnum.Length - i].ToString()] + " " + units[strnum[strnum.Length - (i - 1)].ToString()];
+                        }
+                        break;
+                    case 3:
+                        if (units.ContainsKey(strnum[i - 1].ToString()))
+                        {
+                            answer += units[strnum[strnum.Length - i].ToString()] + " hundered and ";
+                        }
+                        break;
+                    case 4:
+                        if (units.ContainsKey(strnum[i - 1].ToString()))
+                        {
+                            answer += units[strnum[strnum.Length - i].ToString()] + " thusand ";
+                        }
+                        break;
+                    case 1:
+                        if (strnum.Length == 1)
+                        {
+                            answer += units[strnum[0].ToString()];
+                        }
+                        break;
+                    default:
+                        answer += ".";
+                        break;
+                }
+            }
+            return answer;
+        }
+
+    }
+    
+    class WithMaths
+    {
+
+        public string calculate(int strnum)
+        {
+            string answer = "";
+            int thousand = (strnum / 1000);
+            int hundered = (strnum % 1000) / 100;
+            int tens = ((strnum % 1000) % 100) / 10;
+            int unit = ((strnum % 1000) % 100) % 10;
+
+            if (tens == 1)
+            {
+                answer = numToStr(thousand) + " thousand " + numToStr(hundered) + " hundred and " + numToStr(10 + unit); 
+            }
+            else
+            {
+                answer = numToStr(thousand) + " thousand " + numToStr(hundered) + " hundred and " + numToStr(tens * 10) +" " + numToStr(unit); 
+            }
+            
+            return answer;
+        }
+
+        public string numToStr(int strnum)
+        {
+            switch (strnum)
+            {
+                case 0: return "";
+                case 1: return "one";
+                case 2: return "two";
+                case 3: return "three";
+                case 4: return "four";
+                case 5: return "five";
+                case 6: return "six";
+                case 7: return "seven";
+                case 8: return "eight";
+                case 9: return "nine";
+                case 10: return "ten";
+                case 11: return "eleven";
+                case 12: return "twelve";
+                case 13: return "threteen";
+                case 14: return "fourteen";
+                case 15: return "fifteen";
+                case 16: return "sixteen";
+                case 17: return "seventeen";
+                case 18: return "eighteen";
+                case 19: return "nineteen";
+                case 20: return "twenty";
+                case 30: return "thirty"; 
+                case 40: return "fourty";
+                case 50: return "fifty";
+                case 70: return "seventy";
+                case 80: return "eighty";
+                case 90: return "ninty";
+                default : return ".";
+            }
+                
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            WithString changestr = new WithString();
+            WithMaths changemth = new WithMaths();
+            change givechange = new change();
+
+            while (1 == 1)
+            {
+                Console.WriteLine("How much did it cost?");
+                string scanner = Console.ReadLine();
+                
+
+                if (scanner != "exit")
+                {
+                    Console.WriteLine("How much did the customer give?");
+                    double given = Double.Parse(scanner);
+                    double cost = Double.Parse(Console.ReadLine());
+                    Console.WriteLine(givechange.giveChange(Double.Parse(scanner), cost));
+                    //Console.WriteLine(changemth.calculate(Int32.Parse(scanner)));
+                    //Console.WriteLine(change.render(scanner));
+
+                }
+                else
+                {
+                    break;
+                }
+            }
+        }
+    }
+}
---
+++ NumberReader/NumberReader/change.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NumberReader
+{
+    class change
+    {
+
+        public double giveChange(double cost, double given)
+        {
+            if (cost > given)
+            {
+                Console.WriteLine("more money is required");
+            } 
+            else
+            {
+                double leftover = given - cost;
+
+                Console.WriteLine("Total change is £" + leftover);
+                if (leftover >= 20)
+                {
+                    Console.WriteLine( "£20      :"+ Math.Floor(leftover / 20));
+                    leftover = leftover % 20;
+                }
+                if (leftover >= 10)
+                {
+                    Console.WriteLine("£10      :" + Math.Floor(leftover / 10));
+                    leftover = leftover % 10;
+                }
+                if (leftover >= 5)
+                {
+                    Console.WriteLine("£5       :" + Math.Floor(leftover / 5));
+                    leftover = leftover % 5;
+                }
+                if (leftover >= 2)
+                {
+                    Console.WriteLine("£2       :" + Math.Floor(leftover / 2));
+                    leftover = leftover % 2;
+                }
+                if (leftover >= 1)
+                {
+                    Console.WriteLine("£1       :" + Math.Floor(leftover / 1));
+                    leftover = leftover % 1;
+                }
+                if (leftover >= 0.5)
+                {
+                    Console.WriteLine("50p      :" + Math.Floor(leftover / 0.5));
+                    leftover = leftover % 0.5;
+                }
+                if (leftover >= 0.2)
+                {
+                    Console.WriteLine("20p      :" + Math.Floor(leftover / 0.2));
+                    leftover = leftover % 0.2;
+                }
+                if (leftover >= 0.1)
+                {
+                    Console.WriteLine("10p      :" + Math.Floor(leftover / 0.1));
+                    leftover = leftover % 0.1;
+                }
+                if (leftover >= 0.05)
+                {
+                    Console.WriteLine("5p       :" + Math.Floor(leftover / 0.05));
+                    leftover = leftover % 0.05;
+                }
+                if (leftover >= 0.02)
+                {
+                    Console.WriteLine("2p       :" + Math.Floor(leftover / 0.02));
+                    leftover = leftover % 0.02;
+                }
+                if (leftover >= 0.01)
+                {
+                    Console.WriteLine("1p       :" + Math.Floor(leftover / 0.01));
+                    leftover = leftover % 0.01;
+                }
+
+            }
+            
+            return 0.0;
+        }
+    }
+}
