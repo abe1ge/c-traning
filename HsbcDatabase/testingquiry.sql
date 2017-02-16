49def208b1520f7a032ab53e30f88c42a6a2a7d7
abe1ge <abe1ge@github.com>
2017-02-16 17:35:35 +0000

    did a little bit of js and html

--- HsbcDatabase/testingquiry.sql
+++ HsbcDatabase/testingquiry.sql
@@ -1,3 +1,5 @@
-﻿--select accountNo from Accounts where Substring(accountNo, 1, 1) = 'a';
-
-select MAX( SUBSTRING(accountNo, 3,3)) as topAccount from Accounts where Substring(accountNo, 1, 1) = 'a';
+﻿--select Right('000' + CONVERT(NVARCHAR, (MAX( SUBSTRING(accountNo, 3,3)) + 1)), 3)  as topAccount from Accounts where Substring(accountNo, 1, 1) = 'c'
+--insert into Accounts values('SF'+ 
+--(select Right('000' + CONVERT(NVARCHAR, COALESCE((MAX( SUBSTRING(accountNo, 3,3)) + 1),1)), 3)  as topAccount from Accounts where accountNo like 's%','dev','he d live')
+--insert into Accounts values('SF'+ (select Format (COALESCE((MAX( SUBSTRING(accountNo, 3,3)) + 1),1), '000')  as topAccount from Accounts where accountNo like 's%'),'dev','he d li
+--update Accounts set name = 'd dev' where accountNo = 'SF001' or   accountNo = 'CF001'
